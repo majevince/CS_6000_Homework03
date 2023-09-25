8c3d328c40d0d35ceb5c4613fad9d19e566f2208
Vincent Anyah <lptxanyahvincent@gmail.com>
2023-09-25 12:38:57 -0600

    update dataset

---
+++ dataset2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+dataset = pd.read_csv('/home/vincent/Computer_science/fall2023/principle_media_art/homework02/ev_authentication.csv')
+
+
+print(dataset)
